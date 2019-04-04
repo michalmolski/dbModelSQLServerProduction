5f17c30bdef225c227fac3a7e8dac689ff8f7c67
michalmolski <michalmateuszmolski@gmail.com>
2019-04-04 12:25:39 +0200

    ADD PRODUCTION TABLE

--- production.sql
+++ production.sql
@@ -1,4 +1,15 @@
 --============/PRODUCTION TABLES/==============================
 
-
+-- planned production table--
+CREATE TABLE Planned_production
+	(idplan INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	iddetail INT NOT NULL,
+	idmachine INT NOT NULL,
+	plannedstartd DATE,
+	plannedendd DATE,
+	inproduction BIT
+	);
 --============/PRODUCTION CONSTRAINTS -> FOREIGN KEYS==========
+--planned_production FOREING KEYES--------
+ALTER TABLE Planned_production ADD CONSTRAINT FK_Order_detail
+FOREIGN KEY (iddetail) REFERENCES Order_detail(iddetail)
